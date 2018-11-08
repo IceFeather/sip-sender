251f702488a4fd0d69aab26026782b0463f1d73d
IceFeather <benjamin.marlier@gmail.com>
2018-11-08 22:40:24 +0100

    Initial commit

---
+++ src/main/java/fr/icefeather/sip/sender/App.java
@@ -0,0 +1,12 @@
+package fr.icefeather.sip.sender;
+
+import javax.swing.*;
+
+public class App {
+    private JTextField textField1;
+    private JTextField textField3;
+    private JTextField textField4;
+    private JTextField textField5;
+    private JButton sendButton;
+    private JEditorPane editorPane1;
+}
